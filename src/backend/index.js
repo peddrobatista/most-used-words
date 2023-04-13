80d803cc9a82d15687fb07adbf1a2d29f6bbf925
pedro-batista <peddrobatista2022@gmail.com>
2023-04-13 14:36:35 -0300

    Enviando atualizações...

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,10 +1,12 @@
 const {ipcMain} = require("electron");
 const pathToRows = require("./pathToRows");
+const prepareData = require("./prepareData");
 
 ipcMain.on("process-subtitles", (event, paths) => {
     console.log(paths)
     pathToRows(paths)
-    .then(rows => console.log(rows))
+    .then(rows => prepareData(rows))
+    .then(prepareData => console.log(prepareData))
     .then(() => {
         event.reply("process-subtitles", [
             {word: "i", amount: 547},
